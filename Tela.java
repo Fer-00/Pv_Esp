af65aad39114c7d60e7f3eee712e2e5004da3a6d
Adriane Ferreira <drica.jag@outlook.com>
2020-12-16 23:53:43 -0300

    Fim Tela 1 - Por Enquanto

--- Tela.java
+++ Tela.java
@@ -2,7 +2,8 @@ import javax.swing.JFrame;
 
 public class Tela
 {
-	public static void main(String[] args) {
+	public static void main(String[] args)
+	{
 			Tela1 t = new Tela1();
 			t.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);	
 			t.setSize(500,650);
